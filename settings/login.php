ccca64f6fcd00745a2e603f0c354a309e8ac30e3
Alysson Guimarães <->
2025-11-28 16:58:27 -0300

    Feat:Criação de novas class, é organização do codigo, retirando elementos de teste

--- settings/login.php
+++ settings/login.php
@@ -1,13 +1,36 @@
 <?php 
-include('conf_bd.php');
+/*include('conf_bd.php');
 //Conexão com o banco de dados
-$conn = conexao_banco();
+$conn = conexao_banco();*/
 session_start();
 
-$email = $_POST['email'];
+$usuario = $_POST['usuario'];
 $password = $_POST['password'];
 
-var_dump($email);
+var_dump($usuario);
+
+//Verificação de Login para teste
+
+//ler um json para busca um valor
+$login_json = json_decode(file_get_contents('login-config.json'), true);
+// Percorre as contas do JSON
+
+
+foreach ($login_json as $conta) {
+    if ($usuario === $conta['usuario'] && $password === $conta['password']) {
+        header("location: ../home.php");
+        exit();
+    } else {
+        echo "Falha no login. Verifique suas credenciais.";
+    }
+}
+
+
+
+
+
+//Verificação de login via Banco de dados
+/*var_dump($email);
 
 $veri_login = $conn->prepare("SELECT * FROM user WHERE email = ? AND senha = ?");
 
@@ -31,7 +54,7 @@ if($email === $result_credencial['email'] && $password === $result_credencial['s
 }
 
 $conn->close();
-}
+}*/
 
 
 ?>
