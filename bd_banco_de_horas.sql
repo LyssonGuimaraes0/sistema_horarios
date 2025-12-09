faea556e3dd103dd437f876e38314c9f9416e279
Alysson Guimarães <->
2025-12-09 12:45:22 -0300

    backup do banco de dados

--- bd_banco_de_horas.sql
+++ bd_banco_de_horas.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: banco_de_horas
 -- ------------------------------------------------------
@@ -146,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-12-01  0:20:37
+-- Dump completed on 2025-12-09 12:41:06
