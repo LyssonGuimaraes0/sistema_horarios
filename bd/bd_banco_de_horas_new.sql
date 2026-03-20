491fe7b396ce6e343aee57af0c6a1330c7e68746
Alysson Guimarães <->
2026-03-20 17:04:16 -0300

    Fix:Envio base de dados

--- bd/bd_banco_de_horas_new.sql
+++ bd/bd_banco_de_horas_new.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: banco_de_horas
+-- Host: 10.28.0.4    Database: bd_banco_de_horas
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.32-MariaDB
+-- Server version	8.0.41
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,34 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `cargo`
+--
+
+DROP TABLE IF EXISTS `cargo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cargo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `cargo` varchar(16) DEFAULT NULL,
+  `cargo_entrada` time DEFAULT NULL,
+  `cargo_saida_almoco` time DEFAULT NULL,
+  `cargo_volta_almoco` time DEFAULT NULL,
+  `cargo_saida` time DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cargo`
+--
+
+LOCK TABLES `cargo` WRITE;
+/*!40000 ALTER TABLE `cargo` DISABLE KEYS */;
+INSERT INTO `cargo` VALUES (1,'Servidor Público','08:30:00','12:00:00','13:30:00','18:00:00'),(2,'PPE','08:00:00','12:00:00','13:00:00','17:00:00'),(3,'Estágiario-Manha','08:30:00',NULL,NULL,'12:00:00'),(4,'Estágiario-Tarde','13:00:00',NULL,NULL,'17:30:00');
+/*!40000 ALTER TABLE `cargo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `documento_justificativa`
 --
 
@@ -23,13 +51,13 @@ DROP TABLE IF EXISTS `documento_justificativa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `documento_justificativa` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `caminho_justificativa` varchar(255) DEFAULT NULL,
-  `descricao_motivo` text DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `caminho_justificativa` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `descricao_motivo` text COLLATE utf8mb4_general_ci,
   `data_inicio` date NOT NULL,
   `data_fim` date NOT NULL,
-  `data_upload` timestamp NOT NULL DEFAULT current_timestamp(),
+  `data_upload` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `usuario_id` (`usuario_id`),
   CONSTRAINT `documento_justificativa_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
@@ -54,17 +82,17 @@ DROP TABLE IF EXISTS `folha_ponto_mensal`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `folha_ponto_mensal` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
-  `mes` tinyint(4) NOT NULL,
-  `ano` smallint(6) NOT NULL,
-  `caminho_folha_de_ponto` varchar(255) NOT NULL,
-  `data_upload` timestamp NOT NULL DEFAULT current_timestamp(),
-  `observacao_fechamento` text DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
+  `mes` tinyint NOT NULL,
+  `ano` smallint NOT NULL,
+  `caminho_folha_de_ponto` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `data_upload` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `observacao_fechamento` text COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_folha_mes` (`usuario_id`,`mes`,`ano`),
   CONSTRAINT `folha_ponto_mensal_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -73,6 +101,7 @@ CREATE TABLE `folha_ponto_mensal` (
 
 LOCK TABLES `folha_ponto_mensal` WRITE;
 /*!40000 ALTER TABLE `folha_ponto_mensal` DISABLE KEYS */;
+INSERT INTO `folha_ponto_mensal` VALUES (1,1,3,2026,'./docs\\id_1_alysson_andrade_guimaraes\\ponto_mensal\\folha_ponto_id_1_alysson_andrade_guimaraes_03_2026.pdf','2026-03-20 17:13:43',NULL);
 /*!40000 ALTER TABLE `folha_ponto_mensal` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,19 +113,19 @@ DROP TABLE IF EXISTS `ponto_diario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ponto_diario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `usuario_id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `usuario_id` int NOT NULL,
   `data_completo` date DEFAULT NULL,
   `entrada` time DEFAULT NULL,
   `saida_almoco` time DEFAULT NULL,
   `volta_almoco` time DEFAULT NULL,
   `saida` time DEFAULT NULL,
-  `status_dia` varchar(50) DEFAULT 'Em Andamento',
-  `data_registro` timestamp NOT NULL DEFAULT current_timestamp(),
+  `status_dia` varchar(50) COLLATE utf8mb4_general_ci DEFAULT 'Em Andamento',
+  `data_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `unique_ponto_dia` (`usuario_id`,`data_completo`),
   CONSTRAINT `ponto_diario_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +134,7 @@ CREATE TABLE `ponto_diario` (
 
 LOCK TABLES `ponto_diario` WRITE;
 /*!40000 ALTER TABLE `ponto_diario` DISABLE KEYS */;
-INSERT INTO `ponto_diario` VALUES (1,1,'2026-01-01','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 18:49:13'),(3,1,'2026-01-02','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 19:13:12'),(4,1,'2026-01-05','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 19:13:12'),(8,1,'2026-01-07','08:00:00','13:00:00','14:50:00','17:00:00','Completo','2026-01-08 01:59:44'),(13,1,'2026-01-29','08:10:00','13:11:00','14:50:00','18:00:00','Completo','2026-01-09 23:38:06'),(14,1,'2026-01-30','08:08:00','12:00:00','13:00:00','17:50:00','Completo','2026-01-09 23:38:06'),(15,1,'2026-01-08','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13'),(16,1,'2026-01-09','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13'),(17,1,'2026-01-12','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13');
+INSERT INTO `ponto_diario` VALUES (1,1,'2026-01-01','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 18:49:13'),(3,1,'2026-01-02','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 19:13:12'),(4,1,'2026-01-05','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-01-07 19:13:12'),(8,1,'2026-01-07','08:00:00','13:00:00','14:50:00','17:00:00','Completo','2026-01-08 01:59:44'),(13,1,'2026-01-29','08:10:00','13:11:00','14:50:00','18:00:00','Completo','2026-01-09 23:38:06'),(14,1,'2026-01-30','08:08:00','12:00:00','13:00:00','17:50:00','Completo','2026-01-09 23:38:06'),(15,1,'2026-01-08','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13'),(16,1,'2026-01-09','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13'),(17,1,'2026-01-12','08:00:00','12:00:00','13:00:00','17:00:00','Atestado','2026-01-10 00:53:13'),(18,1,'2026-03-20','08:00:00','12:00:00','13:00:00','17:00:00','Completo','2026-03-20 14:15:13'),(26,1,'2026-01-13','08:00:00','12:00:00',NULL,NULL,'Em Andamento','2026-03-20 17:09:47');
 /*!40000 ALTER TABLE `ponto_diario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,17 +146,20 @@ DROP TABLE IF EXISTS `usuario`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `cpf` varchar(14) NOT NULL,
-  `setor` char(7) DEFAULT NULL,
-  `permissoes` enum('administrador','usuario') DEFAULT NULL,
-  `username` varchar(100) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `senha` varchar(250) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `cpf` varchar(14) COLLATE utf8mb4_general_ci NOT NULL,
+  `setor` char(7) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `permissoes` enum('administrador','usuario') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `username` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `senha` varchar(250) COLLATE utf8mb4_general_ci NOT NULL,
+  `cargo` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  UNIQUE KEY `cpf` (`cpf`),
+  KEY `fk_usuario_cargo` (`cargo`),
+  CONSTRAINT `fk_usuario_cargo` FOREIGN KEY (`cargo`) REFERENCES `cargo` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +168,7 @@ CREATE TABLE `usuario` (
 
 LOCK TABLES `usuario` WRITE;
 /*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Alysson Andrade Guimaraes','087.615.015.65','COINF','administrador','alyssonandrade','alysson@gmail.com','123');
+INSERT INTO `usuario` VALUES (1,'Alysson Andrade Guimaraes','087.615.015.65','COINF','administrador','alyssonandrade','alysson@gmail.com','123',2),(2,'Israel Sales','692.803.760-12','COINF','administrador','israelsales','israel@gmail.com','123',1),(3,'Bruno Lins','498.812.430-41','COINF','usuario','brunolins','brunnolis@gmail.com','123',4);
 /*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -149,4 +181,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-01-09 22:04:14
+-- Dump completed on 2026-03-20 17:04:00
