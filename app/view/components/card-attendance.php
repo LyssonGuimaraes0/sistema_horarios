5e3935c341695c8588b37ea2b13f346026c46a01
Alysson Andrade <alyssonandrade186@gmail.com>
2026-06-26 14:59:42 -0300

    Fix: Correção de inputs de card-attendance

--- app/view/components/card-attendance.php
+++ app/view/components/card-attendance.php
@@ -1,3 +1,11 @@
+<!--Chama Helper Permission--->
+
+<?php
+
+use App\helpers\PermissionHelper;
+
+?>
+
 <template id="input-Horarios">
     <div class="linha-dia">
         <div class="container-horarios">
@@ -15,23 +23,32 @@
                 <span>Entrada</span>
                 <input class="horario-input" step="60" type="time" name="entrada">
             </div>
-            <div class="items-horarios input-colunm">
-                <span>Saida Almoco</span>
-                <input class="horario-input" step="60" type="time" name="saida_almoco" >
-            </div>
 
-            <div class="items-horarios input-colunm">
-                <span>Entrada Almoco</span>
-                <input class="horario-input" step="60" type="time" name="volta_almoco" >
-            </div>
+            <!--------------------------------->
+
+            <!--Verifica se os usuarios é Servidor Publico e adiciona novos horarios-->
+
+            <?php if (PermissionHelper::isServidorPublico($user)): ?>
+
+                <div class="items-horarios input-colunm">
+                    <span>Saida Almoco</span>
+                    <input class="horario-input" step="60" type="time" name="saida_almoco">
+                </div>
+
+                <div class="items-horarios input-colunm">
+                    <span>Entrada Almoco</span>
+                    <input class="horario-input" step="60" type="time" name="volta_almoco">
+                </div>
+
+            <?php endif ?>
 
             <div class="items-horarios input-colunm">
                 <span>Saida</span>
-                <input class="horario-input" step="60" type="time" name="saida" >
+                <input class="horario-input" step="60" type="time" name="saida">
             </div>
 
             <div class="items-botoes">
-                <button type='submit' class='btn-calendario' name='dia' data-action="submit" >Confirmar</button>
+                <button type='submit' class='btn-calendario' name='dia' data-action="submit">Confirmar</button>
                 <i class='fa-solid fa-file-alt botao-calendario' data-action="certificate"></i>
             </div>
         </div>
